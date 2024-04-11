1ae76ae449ddfbe9ac89a677748deb73808537b8
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-04-11 12:15:13 -0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,74 @@
+fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`)
+    .then(response => response.json())
+    .then(loadPokemons)
+    .catch(error => console.log(error))
+
+async function loadPokemons(response) {
+    const pokemons = response.results
+    for (let i in pokemons) {
+        const pokeResponse = await fetch(pokemons[i].url);
+        const pokemon = await pokeResponse.json();
+        const specieResponse = await fetch(pokemon.species.url);
+        pokemon.species = await specieResponse.json();
+        showPokemons(pokemon);
+    }
+}
+
+
+function showPokemons(pokemon) {
+    const pokemonListE1 = document.getElementById("pokemons");
+    const name = pokemon.name;
+    const weight = pokemon.weight / 10;
+    const height = pokemon.height / 10;
+    const id = `${pokemon.id}`.padStart(4, '0');
+    const tags = pokemon.types.map((item) => {
+        return item.type.name;
+    });
+
+    const tag = tags[0];
+    let image = pokemon.sprites.front_default
+    if (pokemon.sprites.other["official-artwork"]) {
+        image = pokemon.sprites.other["official-artwork"].front_default
+    }
+
+    const flavor_text = pokemon.species.flavor_text_entries.find(item => {
+        if (item.language.name == "en") {
+            return true;
+        }
+
+        return false;
+    });
+
+    const text = flavor_text.flavor_text
+        .replace("\n", "")
+
+
+
+    console.log(pokemon);
+    pokemonListE1.innerHTML += `
+        <article class="pokemon ${tag}">
+                <div>
+                    <div class="info-content">
+                        <h2>${name}</h2>
+                        <span>${text}</span>
+                        <p><strong>Altura</strong> ${height}m</p>
+                        <p><strong>Peso</strong> ${weight}kg</p>
+
+                        <div class="element-content">
+                        ${tags.map(tag =>
+        `<div class="element ${tag}-element">
+                                    ${tag}
+                                </div>`
+    ).join("")
+        }
+  
+                        </div>
+                    </div>
+                </div>
+                <div class="id">
+                    <span>#${id}</span>
+                    <img src="${image}" alt="${name}">
+                </div>
+        </article>
+    `
+}
